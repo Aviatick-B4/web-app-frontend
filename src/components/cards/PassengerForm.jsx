6632636b0deac5d30f992c86fe5b6506fe4e0a58
talitha <talithasyafiyah1112@gmail.com>
2024-07-04 15:43:43 +0700

    fix: fix validation

--- src/components/cards/PassengerForm.jsx
+++ src/components/cards/PassengerForm.jsx
@@ -60,7 +60,9 @@ function FormPenumpang({ title, fields, formData, handleInputChange }) {
 
   const validateInput = (name, value, ageGroup) => {
     let error = "";
-    if (name.includes("birthDate")) {
+    if (!value) {
+      error = "Data kosong, silahkan isi data diri anda!";
+    } else if (name.includes("birthDate")) {
       const age = calculateAge(value);
       if (ageGroup === "ADULT" && age < 17) {
         error = "Usia dewasa harus lebih dari 17 tahun.";
